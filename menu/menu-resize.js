96c283ce3700088c2374d829734f7a4036274142
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-08-03 22:09:58 -0500

    Try to fix the delay in CSS transitions

--- menu/menu-resize.js
+++ menu/menu-resize.js
@@ -1,11 +1,13 @@
 $(document).on("scroll",function(){
     if($(document).scrollTop()>100){
-        $("nav").removeClass("menu-container").addClass("menu-container-small");
+
         $("ul.menu-list").removeClass("menu-list").addClass("menu-list-small");
         $("li.menu-item").removeClass("menu-item").addClass("menu-item-small");
+        $("nav").removeClass("menu-container").addClass("menu-container-small");
     } else{
-        $("nav").removeClass("menu-container-small").addClass("menu-container");
+
         $("ul.menu-list-small").removeClass("menu-list-small").addClass("menu-list");
         $("li.menu-item-small").removeClass("menu-item-small").addClass("menu-item");
+        $("nav").removeClass("menu-container-small").addClass("menu-container");
     }
 });
